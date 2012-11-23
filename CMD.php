1f474bb882084b47f7db8663fac3b6a9fc296e81
Krayzar <krayzar@gmail.com>
2012-11-23 15:18:51 -0500

    Character whitelist and output file removal
    Still wildly dangerous.
    Regex used = /^.*[0-9|a-z|\$|\/|&|:|\\\|"|\.|\?|\*]$/',
    
    Next up, a simple password system and a whitelist by command.

--- CMD.php
+++ CMD.php
@@ -1,14 +1,15 @@
 <?php
 /* WARNING - DO NOT USE THIS SCRIPT ON "PRODUCTION" WINDOWS WEB SERVERS */
 $Command = $_POST["Command"];
+$head = "<head>\n<title>CMD</title>\n</head>";
+$goback_js = "<script type=\"text/javascript\">\nfunction goBack(){\nwindow.history.back()\n}\n</script>\n";
+$newcom_button = "<input type=\"button\" value=\"New Command\" onclick=\"goBack()\" /><br /><br />";
 
 if (!isset($_POST["Submit"])) { // If page is not submitted to self echo form.
 
 	print (
-		 	"<head>
-		 	<title>CMD</title>
-		 	</head>
-		 	<body>
+			"$head
+			<body>
 		 	<form method=\"post\" action=\"$PHP_SELF\">
   		 	C:\>&nbsp&nbsp&nbsp<input type=\"text\" size=\"50\" maxlength=\"120\" name=\"Command\"/><br />
   		 	<input type=\"Submit\" value=\" Run \" name=\"Submit\"/>
@@ -18,26 +19,28 @@ if (!isset($_POST["Submit"])) { // If page is not submitted to self echo form.
   
 }else{ // Run as if from the windows command prompt, end after - output in textfile (needs to be system writable).
 	
-  		exec("cd\ & c: & cmd.exe /c $Command >" . dirname(__FILE__) . "\outputlog.txt 2>&1");  // No validation - hilariously dangerous!
-	
+	if (preg_match('/^.*[0-9|a-z|\$|\/|&|:|\\\|"|\.|\?|\*]$/', $Command)) {  // Character whitelist validation - still wildly dangerous!
+		
+		exec("cd\ & c: & cmd.exe /c $Command >" . dirname(__FILE__) . "\outputlog.txt 2>&1");  
+		
+	}else{
+		
+		die("$head\n$goback_js\nCommand not allowed<br /><br />$newcom_button");
+		
+	}
+
 	print ( // Display the results of the command line by line - display new command button (just a browser back, a cheat to get the previous command to populate without work).
- 		 	"<head>
-		 	<title>CMD</title>
-			<script type=\"text/javascript\">
- 			function goBack()
-   			{
-   			window.history.back()
-   			}
- 			</script>
-			</head>
+ 		 	"$head
+			$goback_js
 			<body>
-			<b>$Command results</b>&nbsp&nbsp&nbsp<input type=\"button\" value=\"New Command\" onclick=\"goBack()\" /><br /><br />"
+			<b>$Command results</b>&nbsp&nbsp&nbsp<br />$newcom_button"
   		);
   
 $lines = file("outputlog.txt");
   	foreach ($lines as $line_num => $line) {
     print ("$line <br />\n</body>");
 	}
-
+	
+unlink("outputlog.txt");
 }
 ?>
