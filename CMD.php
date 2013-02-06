808fa2d8c3a6ab46eb944bec4c55661cd21b1df2
Krayzar <krayzar@gmail.com>
2013-02-05 23:57:40 -0500

    Added rudimentary session based authentication
    It has a timeout of 1 minute (still testing!) and is designed to keep
    your co-workers and friends out.  As it stands it will not prevent
    someone who has access to your server's file system from getting the
    credentials, and it will not prevent session based attacks.
    
    You could implement some protection from session fixation rather easily
    though by inserting this on line 30... I think...
    
    if (!isset($_SESSION['CREATED'])) {
    $_SESSION['CREATED'] = time();
    } else if (time() - $_SESSION['CREATED'] > ($session_duration_in_ mins *
    60) {
    session_regenerate_id(true);
    $_SESSION['CREATED'] = time();
    }
    
    Next up - "templatitizing" the html for a try at pseudo-MVC in a
    standalone script, and making a user editable config section!

--- CMD.php
+++ CMD.php
@@ -1,9 +1,40 @@
 <?php
+header("Expires: Sat, 02 Apr 1994 00:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Cache-Control: post-check=0, pre-check=0",false);
+header("Pragma: no-cache");
+session_cache_limiter("public, no-store");
+session_start();
 /* WARNING - DO NOT USE THIS SCRIPT ON "PRODUCTION" WINDOWS WEB SERVERS */
+
+$User = "Test";
+$Password = "Test";
+
 $Command = $_POST["Command"];
 $head = "<head>\n<title>CMD</title>\n</head>";
-$goback_js = "<script type=\"text/javascript\">\nfunction goBack(){\nwindow.history.back()\n}\n</script>\n";
-$newcom_button = "<input type=\"button\" value=\"New Command\" onclick=\"goBack()\" /><br /><br />";
+$newcom_button = "<input type=\"button\" value=\"New Command\" onclick=\"window.history.back()\" /><br /><br />";
+
+
+
+if (!isset($_SERVER['PHP_AUTH_USER']) or ($_SESSION['AUTH'] != 1) or ($_SESSION['LAST_ACTIVITY'] && (time() - $_SESSION['LAST_ACTIVITY'] > (1 * 60)))) {
+	$_SESSION['AUTH'] = 1;
+	$_SESSION['LAST_ACTIVITY'] = time();
+    header('WWW-Authenticate: Basic realm="Please Login"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo "You must login to use this page\n\n<input type=\"button\" value=\"Login\" onclick=\"location.reload(forceGet)\" />";
+    die;
+	
+} else {
+
+	if ($_SERVER['PHP_AUTH_USER'] == $User && $_SERVER['PHP_AUTH_PW'] == $Password) {
+	$_SESSION['LAST_ACTIVITY'] = time();
+	} else {
+		echo "Incorrect Password\n<input type=\"button\" value=\"Login\" onclick=\"location.reload(forceGet)\" />";
+		$_SESSION = array();
+		session_destroy();
+		die;
+	}
+}
 
 if (!isset($_POST["Submit"])) { // If page is not submitted to self echo form.
 
@@ -21,17 +52,16 @@ if (!isset($_POST["Submit"])) { // If page is not submitted to self echo form.
 	
 	if (preg_match('/^.*[0-9|a-z|\$|\/|&|:|\\\|"|\.|\?|\*]$/', $Command)) {  // Character whitelist validation - still wildly dangerous!
 		
-		exec("cd\ & c: & cmd.exe /c $Command >" . dirname(__FILE__) . "\outputlog.txt 2>&1");  
+		exec("cd\ & c: & cmd.exe /c $Command >" . dirname(__FILE__) . "\outputlog.txt 2>&1");
 		
 	}else{
 		
-		die("$head\n$goback_js\nCommand not allowed<br /><br />$newcom_button");
+		die("$head\nCommand not allowed<br /><br />$newcom_button");
 		
 	}
 
 	print ( // Display the results of the command line by line - display new command button (just a browser back, a cheat to get the previous command to populate without work).
  		 	"$head
-			$goback_js
 			<body>
 			<b>$Command results</b>&nbsp&nbsp&nbsp<br />$newcom_button"
   		);
@@ -42,5 +72,9 @@ $lines = file("outputlog.txt");
 	}
 	
 unlink("outputlog.txt");
+/*if ($i = 25){
+		$_SESSION = array();
+		session_destroy();
+}*/
 }
 ?>
